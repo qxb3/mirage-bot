7e647896f58dda988dd51e53fc9c56aff7e74c9e
qxb3 <toolong972@gmail.com>
2021-11-14 04:43:58 +0800

    Fixed typo

--- commands/about.js
+++ commands/about.js
@@ -8,7 +8,7 @@ module.exports = {
     callback: async () => {
         const embed = new MessageEmbed()
             .setTitle('About')
-            .setDescription('Hello, im qxb3 (Zeven / Toolong from the game) I made this bot to help newbies get started to the game and i find it really annoying to forget items especially the materials needed for thre enchantments, Anyway i hope this bot can hopefully help you to your mirage realms journey!')
+            .setDescription('Hello, im qxb3 (Zeven / Toolong from the game) I made this bot to help newbies get started to the game and i find it really annoying to forget items especially the materials needed for the enchantments, Anyway i hope this bot can hopefully help you to your mirage realms journey!')
             .addFields([
                 { name: '❯ Found a bug?', value: '• Contact me in discord: qxb3#4312' },
                 { name: '❯ Support me', value: '• Paypal: https://www.paypal.me/qxb3' },
