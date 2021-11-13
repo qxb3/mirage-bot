42529279ed0a267e8b008358b28d4956f8501619
qxb3 <toolong972@gmail.com>
2021-11-14 06:54:38 +0800

    Fixed potions command

--- commands/items/potions.js
+++ commands/items/potions.js
@@ -12,55 +12,60 @@ const getCategories = (categories) => {
 
 module.exports = {
     category: 'Items',
-    description: 'A command that helps you find and indentify materials in the game',
+    description: 'A command that helps you find and indentify potions in the game',
 
     callback: async ({ message, args, prefix }) => {
         const username = message.author.username
         const tag = `#${message.author.discriminator}`
         const avatar = `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
 
-        const materialJson = JSON.parse(Buffer.from(fs.readFileSync(process.env.PWD + '/assets/items/materials.json').toString()))
+        const potionJson = JSON.parse(Buffer.from(fs.readFileSync(process.env.PWD + '/assets/items/potions.json').toString()))
         const categories = []
-        materialJson.forEach(material => {
-            categories.push(material.name)
+        potionJson.forEach(potion => {
+            categories.push(potion.name)
         })
 
         //If there is no argument defined
         if (args[0] == undefined) {
             return new MessageEmbed() 
                 .setAuthor(username + tag, avatar)
-                .addField('❯ Usage', `${prefix}materials <material>\n${prefix}materials list - To lists all materials in the game`)
+                .addField('❯ Usage', `${prefix}potions <potion>\n${prefix}potions list - To lists all potions in the game`)
                 .setColor('RED')
         }
 
-        //List the materials
+        //List the potions
         if (ignoreCase.equals(args[0], 'list')) {
             const list = getCategories(categories)
             return new MessageEmbed() 
                 .setAuthor(username + tag, avatar)
-                .addField('❯ Materials', list)
-                .addField('❯ Usage', `${prefix}materials <material>`)
+                .addField('❯ Potions', list)
+                .addField('❯ Usage', `${prefix}potions <potion>`)
                 .setColor('BLUE')
         }
 
-        //Show full details of a material
+        //Show full details of a potion
         if (!ignoreCase.equals(args[0], 'list')) {
             let code = 1
-            materialJson.forEach(material => {
-                if (ignoreCase.equals(args.join(' '), material.name)) {
+            potionJson.forEach(potion => {
+                if (ignoreCase.equals(args.join(' '), potion.name)) {
                     code = 0
 
+                    let effects = ''
+                    potion.effects.forEach(effect => {
+                        effects += `• ${effect}\n`
+                    })
+
                     let monsters = ''
-                    material.monsters.forEach(monster => {
+                    potion.monsters.forEach(monster => {
                         monsters += `• ${monster}\n`
                     })
 
                     const embed = new MessageEmbed()
                         .setAuthor(username + tag, avatar)
                         .addFields([
-                            { name: '❯ Name', value: material.name },
-                            { name: '❯ Price', value: material.price },
-                            { name: '❯ Drops from', value: monsters },
+                            { name: '❯ Name', value: potion.name },
+                            { name: '❯ Effects', value: effects },
+                            { name: '❯ Monsters', value: monsters },
                         ])
                         .setColor('BLUE')
 
@@ -70,12 +75,12 @@ module.exports = {
                 }
             })
 
-            //If the material user typed didn't exist
+            //If the potion user typed didn't exist
             if (code == 1) {
                 const embed = new MessageEmbed()
                     .setAuthor(username + tag, avatar)
-                    .setDescription('Make sure the material you type is valid')
-                    .addField('❯ Usage', `${prefix}materials list - To list all materials in the game` )
+                    .setDescription('Make sure the potion you type is valid')
+                    .addField('❯ Usage', `${prefix}potion list - To list all food in the game` )
                     .setColor('RED')
 
                 return embed
