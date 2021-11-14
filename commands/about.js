490eb74f226f4a86f152a04a1431fa2c9232434c
qxb3 <toolong972@gmail.com>
2021-11-15 04:27:20 +0800

    Updated about command

--- commands/about.js
+++ commands/about.js
@@ -10,6 +10,7 @@ module.exports = {
             .setTitle('About')
             .setDescription('Hello, im qxb3 (Zeven / Toolong from the game) I made this bot to help newbies get started to the game and i find it really annoying to forget items especially the materials needed for the enchantments, Anyway i hope this bot can hopefully help you to your mirage realms journey!')
             .addFields([
+                { name: '❯ Note', value: 'I am gonna discontinue this project for secret reasons, Anyway while im gone the bot are still gonna be up and running and if you want you to help with the bot can go ahead and help me, The source code are gonna be below so you can go ahead and contribute\nGithub: https://github.com/qxb3/mirage-bot' },
                 { name: '❯ Found a bug?', value: '• Contact me in discord: qxb3#4312' },
                 { name: '❯ Support me', value: '• Paypal: https://www.paypal.me/qxb3' },
                 { name: '❯ Special thanks', value: '• Anonym - I used his wiki for the items, weapons and other stuff!\nHis wiki: http://mr.golitsyn.com/' }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { Client, Intents } = require('discord.js')
 const path = require('path')
 const WokCommands = require('wokcommands')
 
+const mongoose = require('mongoose')
+
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -12,24 +14,28 @@ const client = new Client({
     ]
 })
 
-client.on('ready', () => {
-    console.log("The bot is running")
-
+const setBotActivity = (client) => {
     client.user.setActivity({
         type: 'WATCHING',
         name: `On ${client.guilds.cache.size} Servers!!`
     })
-    
+}
+
+client.on('ready', () => {
+    setBotActivity(client)
+ 
+    mongoose.connect(process.env.MONGODB_URI, (err) => {
+        if (err) throw err
+
+        console.log('Connected to mongodb')
+    })
+
     new WokCommands(client, {
-        commandDir: path.join(__dirname, './commands'),
+        commandDir: path.join(__dirname, './commands'), 
         testServers: [
             '811195710065082378', //Tools server
         ]
     }).setDefaultPrefix('?')
-})
-
-client.on('messageCreate', (message) => {
-    console.log(message)
 })
 
 client.login(process.env.BOT_TOKEN)
