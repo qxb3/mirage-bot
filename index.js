abb011ac0d4f8ad773085128ac5f9863bfbb5711
qxb3 <toolong972@gmail.com>
2021-11-27 22:26:39 +0800

    Removed

--- index.js
+++ index.js
@@ -12,45 +12,14 @@ const client = new Client({
     ]
 })
 
-const setBotActivity = (client) => {
-    client.user.setActivity({
-        type: 'WATCHING',
-        name: `On ${client.guilds.cache.size} Servers!!`
-    })
-}
-
 client.on('ready', () => {
-    setBotActivity(client)
-
     new WokCommands(client, {
         commandDir: path.join(__dirname, './commands'), 
-        features: path.join(__dirname, './features'),
+        featuresDir: path.join(__dirname, './features'),
         testServers: ['811195710065082378']
     })
     .setDefaultPrefix('?')
     .setBotOwner('591150858830479381')
 })
-
-client.on("guildCreate", async (guild) => {
-    const embed = new MessageEmbed()
-        .setColor('BLUE')
-        .setThumbnail(guild.iconURL({ dynamic: true }))
-        .setTitle("New Server!")
-        .addField("Server Name", guild.name, true)
-        .addField("Server ID", guild.id, true)
-        .addField("Owner ID", guild.ownerID, true)
-        .addField("Owner Mention", `<@${guild.ownerID}>`, true)
-        .addField("Member Count", guild.memberCount, true)
-        .setFooter(client.user.username, client.config.embed.thumbnail);
-
-        await guild.channels.cache
-        .filter(channel => channel.type === "text") //added this line, should work like a charm
-        .first()
-        .createInvite()
-        .then((invite) => embed.addField("Invite link", invite.url, true))
-        .catch(() => embed.addField("Invite link", "Missing permissions", true));
-
-    channel.send(embed);
-});
 
 client.login(process.env.BOT_TOKEN)
