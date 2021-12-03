1d85744b67a9381a1e1e4a17a62d9f33559315c0
qxb3 <toolong972@gmail.com>
2021-12-03 14:27:05 +0800

    Added ping command

---
+++ commands/bot/ping.js
@@ -0,0 +1,22 @@
+module.exports = {
+    category: 'Bot',
+    description: 'Get the bot latency',
+
+    slash: 'both',
+    testOnly: true,
+
+    callback: async ({ message, interaction }) => {
+        if (message) {
+            await message.channel.send('Calculating latency...').then(result => {
+                const latency = result.createdTimestamp - message.createdTimestamp 
+                result.edit(`This message took me ${latency}ms to send!`)
+            })
+        }
+
+        if (interaction) {
+            const msg = await interaction.reply({ content: 'Calculating latency...', fetchReply: true })
+            const latency = msg.createdTimestamp - interaction.createdTimestamp
+            interaction.editReply(`This message took me ${latency}ms to send!`)
+        }
+    }
+}
