89b9974bda880011c969ffecf791b8940b1135e1
qxb3 <toolong972@gmail.com>
2021-12-09 04:40:58 +0800

    Refactored materials command

--- commands/items/materials.js
+++ commands/items/materials.js
@@ -1,21 +1,14 @@
-const { MessageEmbed } = require('discord.js')
-const ignoreCase = require('ignore-case')
-const fs = require('fs')
-const getMessageDetails = require('../../utils/get-message-details')
-const sendMessage = require('../../utils/send-message')
+const materialsJson = require('@assets/items/materials.json')
+const sendMessage = require('@utils/send-message')
+const formatter = require('@utils/formatter')
+const getEmbed = require('@utils/get-embed')
 
-const getCategories = (categories) => {
-    let list = ''
-    categories.forEach(category => {
-        list += `• ${category}\n`
-    })
-    return list
-}
+const didyoumean = require('didyoumean2').default
 
 module.exports = {
     category: 'Items',
     description: 'A command that will help you with materials in the game.',
-    aliases: ['material', 'mats'],
+    aliases: ['material', 'mats', 'mt'],
 
     slash: 'both',
 
@@ -29,87 +22,67 @@ module.exports = {
         }
     ],
 
-    callback: async ({ message, interaction, args, prefix }) => {
-        const messageDetails = getMessageDetails(message, interaction, prefix)
+    callback: async ({ message, interaction, args, prefix, user }) => {
+        const embed = getEmbed(user)
 
-        const materialJson = JSON.parse(Buffer.from(fs.readFileSync(process.env.PWD + '/assets/items/materials.json').toString()))
-        const categories = []
-        materialJson.forEach(material => {
-            categories.push(material.name)
-        })
-
-        //List the materials
-        if (!args[0]) {
-            const list = getCategories(categories)
-            const embed = new MessageEmbed() 
-                .setAuthor(messageDetails.author, messageDetails.avatar)
-                .setThumbnail('attachment://troll-fur.png')
-                .addField('❯ Materials', list)
-                .addField('❯ Usage', `${messageDetails.prefix}materials <material>`)
-                .setColor('BLUE')
-
-            return sendMessage(message, interaction, {
-                embeds: [
-                    embed
-                ],
-                files: [
-                    process.env.PWD + '/assets/items/sprites/materials/troll-fur.png'
-                ]
-            })
+        if (interaction) {
+            prefix = '/'
         }
 
-        //Show full details of a material
-        let code = 1
-        materialJson.forEach(material => {
-            if (ignoreCase.equals(args.join(' '), material.name)) {
-                code = 0
+        const usage = {
+            name: '❯ Usage',
+            value: `${prefix}materials <material> - To see the full details of the material.\n` +
+                   `${prefix}materials - To list all the materials.`
+        }
 
-                const name = material.name.replaceAll(' ', '-').toLowerCase()
-                const sprite = process.env.PWD + '/assets/items/sprites/materials/' + name + '.png'
+        //If user didn't give arguments
+        if (args.length === 0) {
+            const materials = formatter(materialsJson.map(data => data.name))
 
-                let monsters = ''
-                material.monsters.forEach(monster => {
-                    monsters += `• ${monster}\n`
-                })
-
-                const embed = new MessageEmbed()
-                    .setAuthor(messageDetails.author, messageDetails.avatar)
-                    .setThumbnail(`attachment://${name}.png`)
-                    .addFields([
-                        { name: '❯ Name', value: material.name },
-                        { name: '❯ Price', value: material.price },
-                        { name: '❯ Drops from', value: monsters },
-                    ])
-                    .setColor('BLUE')
-
-                sendMessage(message, interaction, {
-                    embeds: [
-                        embed
-                    ],
-                    files: [
-                        sprite
-                    ]
-                })
-            }
-        })
-
-        //If the material user typed didn't exist
-        if (code == 1) {
-            const embed = new MessageEmbed()
-                .setAuthor(messageDetails.author, messageDetails.avatar)
-                .setThumbnail('attachment://troll-fur.png')
-                .setDescription('Make sure the material you type is valid')
-                .addField('❯ Usage', `${messageDetails.prefix}materials - To list all materials in the game` )
-                .setColor('RED')
+            embed.setThumbnail('attachment://troll-fur.png')
+            embed.addFields([
+                { name: '❯ Materials', value: materials },
+                usage
+            ])
 
             sendMessage(message, interaction, {
-                embeds: [
-                    embed
-                ],
-                files: [
-                    process.env.PWD + '/assets/items/sprites/materials/troll-fur.png'
-                ]
+                embeds: [ embed ],
+                files: [ 'assets/items/sprites/materials/troll-fur.png' ]
             })
+            return
         }
+
+        const input = args.join(' ')
+        const isMaterial = didyoumean(input, materialsJson.map(data => data.name), { threshold: 0.6 })
+
+        //If the user input is material
+        if (isMaterial) {
+            const material = materialsJson.find(data => data.name === isMaterial)
+            const sprite = material.name.replace(/'/, '').replaceAll(' ', '-').toLowerCase() + '.png'
+
+            embed.setThumbnail(`attachment://${sprite}`)
+            embed.addFields([
+                { name: '❯ Name', value: material.name },
+                { name: '❯ Price', value: material.price },
+                { name: '❯ Monsters', value: formatter(material.monsters) },
+            ])
+
+            sendMessage(message, interaction, {
+                embeds: [ embed ],
+                files: [ `assets/items/sprites/materials/${sprite}` ]
+            })
+            return
+        }
+
+        //If didn't match anything
+        embed.setThumbnail('attachment://troll-fur.png')
+        embed.setDescription('The material you typed did not match to any materials.')
+        embed.addFields([ usage ])
+        embed.setColor('RED')
+
+        sendMessage(message, interaction, {
+            embeds: [ embed ],
+            files: [ 'assets/items/sprites/materials/troll-fur.png' ]
+        })
     }
 }
