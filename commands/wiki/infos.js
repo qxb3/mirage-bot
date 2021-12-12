7156f30f2d96df76681544a903f0da8609fd6e99
qxb3 <toolong972@gmail.com>
2021-12-13 03:23:23 +0800

    Added command: info's

---
+++ commands/wiki/infos.js
@@ -0,0 +1,88 @@
+const infosJson = require('@assets/wiki/infos.json')
+
+const sendMessage = require('@utils/send-message')
+const formatter = require('@utils/formatter')
+const getEmbed = require('@utils/get-embed')
+
+const didyoumean = require('didyoumean2').default
+
+module.exports = {
+    category: 'Wiki',
+    description: 'A command for info s about the game (This command content are mostly about skull systems).',
+    aliases: ['info', 'inf'],
+
+    //slash: 'both',
+    testOnly: true,
+
+    expectedArgs: '<info>',
+    /*options: [
+        {
+            name: 'info',
+            description: 'The info name you want to check.',
+            required: false,
+            type: 3
+        }
+    ],*/
+
+    callback: async ({ message, interaction, args, prefix, user }) => {
+         const embed = getEmbed(user)
+
+        if (interaction) {
+            prefix = '/'
+        }
+
+        const usage = {
+            name: '❯ Usage',
+            value: `${prefix}infos <info> - To see the full details of the info.\n` +
+                   `${prefix}infos - To list all the info's available.`
+        }
+
+        //If user didn't give arguments
+        if (args.length === 0) {
+            const infos = formatter(infosJson.map(data => data.name))
+            
+            embed.setThumbnail('attachment://rules.png')
+            embed.addFields([
+                { name: `❯ Info's`, value: infos },
+                usage
+            ])
+
+            sendMessage(message, interaction, {
+                embeds: [ embed ],
+                files: [ 'assets/icons/rules.png' ]
+            })
+            return
+        }
+
+        const input = args.join(' ')
+        const isInfo = didyoumean(input, infosJson.map(data => data.name), { threshold: 0.6 })
+
+        //If the user input is info
+        if (isInfo) {
+            const info = infosJson.find(data => data.name === isInfo)
+
+            embed.setThumbnail(`attachment://rules.png`)
+            embed.addFields([
+                { name: '❯ Name', value: info.name },
+                { name: '❯ Description', value: info.description }
+            ])
+
+            sendMessage(message, interaction, {
+                embeds: [ embed ],
+                files: [ `assets/icons/rules.png` ]
+            })
+            return
+        }
+
+        //If didn't match anything
+        embed.setThumbnail('attachment://rules.png')
+        embed.setDescription('The info you typed did not match to any infos.')
+        embed.addFields([ usage ])
+        embed.setColor('RED')
+
+        sendMessage(message, interaction, {
+            embeds: [ embed ],
+            files: [ 'assets/icons/rules.png' ]
+        })
+    }
+}
