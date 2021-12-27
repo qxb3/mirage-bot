051d89226b84e075c2b5b42378c7509d070d78c4
qxb3 <toolong972@gmail.com>
2021-12-27 15:38:36 +0800

    Increase the timeout to 1sec

--- features/autogz.js
+++ features/autogz.js
@@ -22,7 +22,7 @@ module.exports = (client) => {
                     if (randomMessage) {
                         await message?.reply(randomMessage)
                     }
-                }, 500)
+                }, 1000)
             } 
         } 
     })
