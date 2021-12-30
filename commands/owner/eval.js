961c22320c28158828750d53298aa7703084ae52
qxb3 <toolong972@gmail.com>
2021-12-30 16:14:56 +0800

    Some eval changes

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -11,37 +11,31 @@ module.exports = {
     hidden: true,
 
     callback: async ({ message, args, prefix, instance, client }) => {
-        const embed = new MessageEmbed()
-            .setTimestamp()
-            .setColor('GREEN')
-
-        const input = args.join(' ')
-        if (!input) {
-            embed.setDescription('You need to add a code.')
-            embed.setColor('RED')
-
-            await message.channel.send({
-                embeds: [ embed ]
-            })
-            return
-        }
-
-        let output = ''
+        const input = args.join(' ') || 'None'
         try {
-            output = require('util').inspect(eval(`${input}`))
+            const output = eval(await clean(input, client))
+
+            if (output.length > 1024) output = 'The output is too large!'
+            const embed = new MessageEmbed() 
+                .addFields([
+                    { name: 'INPUT:inbox_tray:', value: quote(input) },
+                    { name: 'OUTPUT:outbox_tray:', value: quote(output) }
+                ])
+                .setTimestamp()
+                .setColor('GREEN')
+
+            await message.channel.send({ embeds: [ embed ] })
         } catch(err) {
-            output = err
-        }
+            const embed = new MessageEmbed() 
+                .addFields([
+                    { name: 'INPUT:inbox_tray:', value: quote(input) },
+                    { name: 'OUTPUT:outbox_tray:', value: quote(err) }
+                ])
+                .setTimestamp()
+                .setColor('RED')
 
-        if (output.length > 1024) output = 'The output is too large!'
-        embed.addFields([
-            { name: 'INPUT:inbox_tray:', value: quote(input) },
-            { name: 'OUTPUT:outbox_tray:', value: quote(output) }
-        ])
-
-        await message.channel.send({
-            embeds: [ embed ]
-        })
+            await message.channel.send({ embeds: [ embed ] })
+        } 
     },
     
     error: () => {}
@@ -49,4 +43,19 @@ module.exports = {
 
 function quote(message) {
     return '```js\n' + message + '\n```'
+}
+
+const clean = async (text, client) => {
+    if (text && text.constructor.name == "Promise")
+        text = await text
+
+    if (typeof text !== "string")
+        text = require("util").inspect(text, { depth: 1 })
+
+    text = text
+        .replace(/`/g, "`" + String.fromCharCode(8203))
+        .replace(/@/g, "@" + String.fromCharCode(8203))
+        .replaceAll(client.token, '')
+
+    return text
 }
