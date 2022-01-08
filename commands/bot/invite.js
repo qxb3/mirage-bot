0c500eef026e2a7f368620cca7fcc97aefd90985
qxb3 <toolong972@gmail.com>
2022-01-08 14:07:30 +0800

    Added button 'direct invite' to invite command

--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -1,14 +1,26 @@
-const sendMessage = require('../../utils/send-message')
+const { MessageActionRow, MessageButton } = require('discord.js')
+
+const sendMessage = require('@utils/send-message')
 
 module.exports = {
     category: 'Bot',
     description: 'Get the bot invite link.',
+    aliases: ['inv'],
 
     slash: 'both',
 
     callback: async ({ message, interaction }) => {
+        const row = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setLabel('Direct Invite')
+                    .setURL('https://discord.com/api/oauth2/authorize?client_id=903372467370864740&permissions=414464788545&scope=bot%20applications.commands')
+                    .setStyle('LINK')
+            )
+
         sendMessage(message, interaction, {
-            content: 'https://discordbotlist.com/bots/miragebot'
+            content: 'https://discordbotlist.com/bots/miragebot',
+            components: [ row ]
         })
     }
 }
