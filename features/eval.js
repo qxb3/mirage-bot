ba98646ee2f92e1590ae4008488086601df6a281
qxb3 <toolong972@gmail.com>
2022-01-10 12:20:01 +0800

    Added utility functions into eval

--- features/eval.js
+++ features/eval.js
@@ -1,5 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 
+const Utils = require('@utils/EvalUtils')
+
 module.exports = (client, instance) => {
     client.on('messageCreate', async (message) => {
         const args = message.content.split(' ')
@@ -31,16 +33,16 @@ module.exports = (client, instance) => {
             return
         }
 
-        const input = args.join(' ') || 'None'
+        const input = args.join(` `) || 'None'
         
         try {
-            let output = clean(await eval(input))
+            let output = Utils.clean(await eval(input))
             if (output.length > 1024) output = 'The output is too large!'
 
             const embed = new MessageEmbed()
                 .addFields([
-                    { name: 'INPUT:inbox_tray:', value: quote(input) },
-                    { name: 'OUTPUT:outbox_tray:', value: quote(output) }
+                    { name: 'INPUT:inbox_tray:', value: Utils.quote(input) },
+                    { name: 'OUTPUT:outbox_tray:', value: Utils.quote(output) }
                 ])
                 .setTimestamp()
                 .setColor('GREEN')
@@ -49,8 +51,8 @@ module.exports = (client, instance) => {
         } catch(err) {
             const embed = new MessageEmbed()
                 .addFields([
-                    { name: 'INPUT:inbox_tray:', value: quote(input) },
-                    { name: 'OUTPUT:outbox_tray:', value: quote(err) }
+                    { name: 'INPUT:inbox_tray:', value: Utils.quote(input) },
+                    { name: 'OUTPUT:outbox_tray:', value: Utils.quote(err) }
                 ])
                 .setTimestamp()
                 .setColor('RED')
@@ -58,27 +60,6 @@ module.exports = (client, instance) => {
             await message.channel.send({ embeds: [ embed ] })
         }
     })
-}
-
-function quote(message) {
-    return '```js\n' + message + '\n```'
-}
-
-const clean = (text) => {
-    /*if (text && text.constructor.name == 'Promise') {
-        text = await text
-    }*/
-
-    if (typeof text !== 'string') {
-        text = require('util').inspect(text, { depth: 1 })
-    }
-
-    text = text.replaceAll('`', '`' + String.fromCharCode(8203))
-    text = text.replaceAll('@', '@' + String.fromCharCode(8203));
-    text = text.replaceAll(process.env.TOKEN, ' ')
-    text = text.replaceAll(process.env.MONGO_URI, ' ')
-
-    return text
 }
 
 module.exports.config = {
---
+++ utils/EvalUtils.js
@@ -0,0 +1,64 @@
+class EvalUtils {
+
+    //Utility Functions
+    static async sendMessage(client, guildId, channelId, message) {
+        const guild = client.guilds.cache.get(guildId)
+        const channel = await guild.channels.fetch(channelId)
+        await channel.send(message)
+    }
+
+    static async replyMessage(client, guildId, channelId, messageId, message) {
+        const guild = client.guilds.cache.get(guildId)
+        const channel = await guild.channels.fetch(channelId)
+        const msg = await channel.messages.fetch(messageId)
+        await msg.reply(message)
+    }
+
+    static getServersSize(client) {
+        return client.guilds.cache.size
+    }
+
+    static getUsersSize(client) {
+        return client.users.cache.size
+    }
+
+    static getTotalUsersSize(client) {
+        let users = 0
+        client.guilds.cache.forEach((guild) => {
+            users += guild.memberCount
+        })
+        return users
+    }
+
+    static list() {
+        return 'sendMessage(client, guildId, channelId, message)\n' +
+               'replyMessags(client, guildId, channelId, messageId, message)\n' +
+               'getServersSize(client)\n' +
+               'getUsersSize(client)\n' +
+               'getTotalUsersSize(client)'
+    }
+
+    //Utils
+    static clean(text) {
+        /*if (text && text.constructor.name == 'Promise') {
+            text = await text
+        }*/
+
+        if (typeof text !== 'string') {
+            text = require('util').inspect(text, { depth: 1 })
+        }
+
+        /*text = text.replaceAll('`', '`' + String.fromCharCode(8203))
+        text = text.replaceAll('@', '@' + String.fromCharCode(8203));*/
+        text = text.replaceAll(process.env.TOKEN, ' ')
+        text = text.replaceAll(process.env.MONGO_URI, ' ')
+
+        return text
+    }
+
+    static quote(message) {
+        return '```js\n' + message + '\n```'
+    }
+}
+
+module.exports = EvalUtils
